bb9742487c015948d059392d8b99d192416f4d3c
Dominik Kesim <Dominik.Kesim@gmail.com>
2022-01-31 16:19:07 +0100

    Added additional headers to avoid cors error

--- routes/stocks.js
+++ routes/stocks.js
@@ -5,7 +5,11 @@ const Stock = require('../models/Stock');
 
 router.use(function (req, res, next) {
     // res.header("Access-Control-Allow-Origin", ["http://localhost:8080"]); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
 
